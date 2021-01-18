cfe8dc47f04d2e932d67e42c0b040953c6c3075b
Samiha Khan <samiha-khan@users.noreply.github.com>
2021-01-18 23:41:59 +0600

    Soldier and Bananas

---
+++ CodeForces-546A.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+        public class Main
+        {
+        	public static void main(String []args)
+        	{
+        		Scanner sc=new Scanner(System.in);
+        		int k=sc.nextInt();
+        		int n=sc.nextInt();
+        		int w=sc.nextInt();
+        		int sum=0;
+        		for(int i=1;i<=w;i++)
+        		{
+        			sum+=(i*k);
+        		}
+        		if(sum<=n)
+        		{
+        			System.out.println("0");
+        		}
+        		else
+        		{
+        			System.out.println(sum-n);
+        		}
+        	}
+        }
+
